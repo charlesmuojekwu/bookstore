68c96c46889ac701967fc610153a0c54bedec52a
Charles Muojekwu <charlesmuojekwu@gmail.com>
2021-12-07 10:41:05 +0100

    Added mvc to application

---
+++ bookstoreproject/bookstoreproject/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bookstoreproject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Charles home controller text";
+        }
+    }
+}
